4c83fb717528291090df2bffa541c2ebdb9ddb84
Dave <dave.turmawan@outlook.com>
2025-03-03 10:39:40 +0700

    Some stuff completed

--- src/modules/subscriptionHandler.ts
+++ src/modules/subscriptionHandler.ts
@@ -1,6 +1,6 @@
 import db from "./db";
 
-function HEXToVBColor(rrggbb) {
+function HEXToVBColor(rrggbb: string) {
     var bbggrr = rrggbb.substr(4, 2) + rrggbb.substr(2, 2) + rrggbb.substr(0, 2);
     return parseInt(bbggrr, 16);
 }
