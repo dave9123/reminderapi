6308c452d810b32f34f807798b0f49db7a66b181
Dave <dave.turmawan@outlook.com>
2025-03-09 19:29:27 +0700

    best

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import helmet from "helmet";
+import fs from "fs";
 import db from "./modules/db";
 dotenv.config();
 
@@ -42,6 +43,30 @@ import subscriptionHandler from "./modules/subscriptionHandler";
 subscriptionHandler();
 
 app.use(express.static("public"));
+
+fs.readFile("public/index.html", "utf8", (err, data) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    const updatedData = data
+        .replace(/<h1><a href=".*?">reminderapi<\/a><\/h1>/, `<h1><a href="${process.env.BASEURL}">reminderapi</a></h1>`)
+        .replace(/<link rel="stylesheet" href=".*?">/, `<link rel="stylesheet" href="${process.env.BASEURL}/assets/css/style.css">`)
+        .replace(
+            /{"@context":"https:\/\/schema\.org","@type":"WebSite","headline":"ReminderAPI","name":"reminderapi","url":".*?"}/,
+            `{"@context":"https://schema.org","@type":"WebSite","headline":"ReminderAPI","name":"reminderapi","url":"${process.env.BASEURL}"}`
+        )
+        .replace(/<meta property="og:url" content=".*?" \/>/, `<meta property="og:url" content="${process.env.BASEURL}" />`)
+        .replace(/<link rel="canonical" href=".*?" \/>/, `<link rel="canonical" href="${process.env.BASEURL}" />`);
+
+    fs.writeFile("public/index.html", updatedData, (err) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on http://localhost:${port}`);
